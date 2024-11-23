d79abc2a8a28bf3f57d1e058cc11fdf70c80fa0a
MarcHoingnet <hoingnetmarc@gmail.com>
2024-11-23 17:14:37 +0100

    gm collectible base c++

---
+++ Source/Cours_CPP/Private/GM_Collectible.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GM_Collectible.h"
+#include "UObject/ConstructorHelpers.h"
+
+AGM_Collectible::AGM_Collectible()
+{
+	
+}
---
+++ Source/Cours_CPP/Public/GM_Collectible.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GM_Collectible.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COURS_CPP_API AGM_Collectible : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+public:
+	AGM_Collectible();
+
+};
