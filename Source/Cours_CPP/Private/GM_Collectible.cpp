9488b03b76b301d8ef4024c5bbda8f6777bac813
MarcHoingnet <hoingnetmarc@gmail.com>
2024-11-30 23:50:07 +0100

    play du titlemap pas de joueur 2

--- Source/Cours_CPP/Private/GM_Collectible.cpp
+++ Source/Cours_CPP/Private/GM_Collectible.cpp
@@ -1,9 +1,10 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
+#include "GM_Collectible.h"
 #include "Blueprint/UserWidget.h"
 #include "Kismet/GameplayStatics.h"
-#include "GM_Collectible.h"
+
 
 
 void AGM_Collectible::BeginPlay()
